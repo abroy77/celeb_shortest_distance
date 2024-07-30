91342afbc8b797193e2f0510e43abd14e2105c06
Abhishek Roy <royabhishek77@gmail.com>
2024-07-30 14:30:20 +0530

    set empty birth_year as null

--- migrations/20240523214128_make_full_text_index.sql
+++ migrations/20240523214128_make_full_text_index.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
 -- Create virtual table
-CREATE VIRTUAL TABLE actors USING fts5(full_name, birth_year UNINDEXED, tokenize="trigram", prefix="5 7 10");
+CREATE VIRTUAL TABLE actors USING fts5(full_name, birth_year UNINDEXED,id UNINDEXED, tokenize="trigram", prefix="5 7 10");
  
--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -6,5 +6,6 @@ CREATE TABLE temp_actors(id INT, full_name TEXT, birth_year INT);
 .separator ","
 .headers on
 .import data/new_large/actors.csv temp_actors
-INSERT INTO actors (full_name, birth_year) SELECT full_name, birth_year FROM temp_actors;
+INSERT INTO actors (full_name, birth_year, id)
+SELECT full_name, CASE WHEN birth_year = '' THEN NULL ELSE birth_year END, id FROM temp_actors;
 EOF
