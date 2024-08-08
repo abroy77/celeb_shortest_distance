33388a8174abd2b868ad47438bf67f7361a9c69f
Abhishek Roy <abhishek.roy@oxfordbraindiagnostics.com>
2024-08-08 09:31:25 +0100

    remove temp_table

--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -8,4 +8,5 @@ CREATE TABLE temp_actors(id INT, full_name TEXT, birth_year INT);
 .import data/new_large/actors.csv temp_actors
 INSERT INTO actors (full_name, birth_year, id)
 SELECT full_name, CASE WHEN birth_year = '' THEN NULL ELSE birth_year END, id FROM temp_actors;
+DROP TABLE temp_actors;
 EOF
